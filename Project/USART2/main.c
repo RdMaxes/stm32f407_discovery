b57270aad0b4dfd6010a33b032d3854109d1c294
inch-huang <yc.huang0522@gmail.com>
2014-06-29 02:00:41 +0800

    finish my_printf() funtion test

--- Project/USART2/main.c
+++ Project/USART2/main.c
@@ -3,7 +3,8 @@
 #include "usart2.h"
 #include "myprintf.h"
 
-uint8_t tx_buf[20] = "Hello Wrold!! iti\n";
+int32_t test_var = 343422;
+
 static void delay(int32_t time)
 {
 	while(time--);
@@ -17,7 +18,7 @@ int main(void)
 	
 	while(1) 
 	{
-		Usart2_DMA_Send(tx_buf,sizeof(tx_buf));
+		my_printf("Hello, variable value is %d\r\n",test_var);
 		delay(8000000);
 		LED_GREEN_ON();
 		delay(8000000);
