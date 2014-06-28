15d0718294f6bfb04013153ba6c4350985597642
inch-huang <yc.huang0522@gmail.com>
2014-06-28 23:05:37 +0800

    delete the wrong directory

--- Pheripherals/usart2/usart2.h
+++
@@ -1,9 +0,0 @@
-#ifndef USART2_H
-#define USART2_H
-
-//function declaration
-void Usart2_Init(int baudrate);
-void DMA2_Usart2_Config(void);
-void Usart2_DMA_Send(int8_t *buf, int16_t len);
-
-#endif
